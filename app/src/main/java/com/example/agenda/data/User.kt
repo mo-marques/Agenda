6d3fe3e7442bba1a3cf5780c9cb70e266d7eefc4
Mônica Marques <mo.menezes.marques@gmail.com>
2021-08-21 12:07:02 -0300

    Create DataBase Schema

---
+++ app/src/main/java/com/example/agenda/data/User.kt
@@ -0,0 +1,14 @@
+package com.example.agenda.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val task: String,
+    val description: String,
+    val date: String,
+    val hour: String,
+)
