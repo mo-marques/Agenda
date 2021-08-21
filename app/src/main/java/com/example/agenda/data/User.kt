7cb1ce8c0f0cb55963f90419c5f17803479a76d7
Mônica Marques <mo.menezes.marques@gmail.com>
2021-08-21 15:26:40 -0300

    commit pre stash

--- app/src/main/java/com/example/agenda/data/User.kt
+++ app/src/main/java/com/example/agenda/data/User.kt
@@ -3,8 +3,7 @@ package com.example.agenda.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "user_table")
-data class User (
+@Entity(tableName = "user_table") data class User (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val task: String,
