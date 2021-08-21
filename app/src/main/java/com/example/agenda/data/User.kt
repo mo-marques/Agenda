ad1350ff44bd4911af3b42e9c82c420e5be3cc34
Mônica Marques <mo.menezes.marques@gmail.com>
2021-08-21 15:30:37 -0300

    resolved merge conflict

--- app/src/main/java/com/example/agenda/data/User.kt
+++ app/src/main/java/com/example/agenda/data/User.kt
@@ -3,11 +3,12 @@ package com.example.agenda.data
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
+
 @Entity(tableName = "user_table") data class User (
     @PrimaryKey(autoGenerate = true)
     val id: Int,
     val task: String,
     val description: String,
     val date: String,
-    val hour: String,
+    val hour: String
 )
